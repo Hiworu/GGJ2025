2226da98ab22bb8c41a15a20e314069960cfd856
Beatrice Verardi <shinybea01@gmail.com>
2025-01-25 14:00:25 +0100

    Updated Player Behavior

--- BubbleGame/BubbleGame/Assets/Scripts/Scripts_bv/PlayerBehavior.cs
+++ BubbleGame/BubbleGame/Assets/Scripts/Scripts_bv/PlayerBehavior.cs
@@ -5,54 +5,93 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-
-    private new Camera _camera;
-    [SerializeField] private int ammo = 0;
+    [SerializeField] private float bobaShootForce = 10f;
+    [SerializeField] private float bobaSpawnTime = 10f;
     
+    private SwitchCamera _switchCamera;
+    private GameObject _bobaPrefab;
+    private Transform _bobaSpawnPoint;
+    
+    private Camera _camera;
+    private int _maxAmmo = 1;
+    private int _ammo = 0;
+    
+    private GameObject _draggedObject;
+    private Vector3 _dragOffset;
+
+    private void Start()
+    { _camera = Camera.main; }
     private void Update()
     {
-        _camera = Camera.main;
+        if(_switchCamera.camera1.enabled) {_camera = _switchCamera.camera1;}
+        if(_switchCamera.camera2.enabled) {_camera = _switchCamera.camera2;}
         
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
-            Vector3 playerDirection = _camera.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(camera.transform.position, playerDirection, out hit))
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 var target = hit.collider.gameObject;
                 //if (target == bubble || topping)
-                //{DragObject()}
-                
+                if (target.CompareTag("Bubble") || target.CompareTag("Topping"))
+                {
+                    StartDragging(target, hit.point);
+                }
                 //if (target == straw)
-                //{SelectStraw();}
+                else if (target.CompareTag("Straw"))
+                {
+                    SelectStraw(target);
+                }
                 //if (ammo > 0)
-                //{ShootBoba()}
+                else if (_ammo > 0)
+                {
+                    ShootBoba();
+                }
             }
         }
-
         //drop
-        if (Input.GetMouseButtonUp(0))
-        {
-            //DropObject()
-        }
+        if (Input.GetButtonUp("Fire1"))
+        { _draggedObject = null; }
 
         //charge straw
         if (Input.GetMouseButtonDown(1))
         {
-            ammo++;
-            if (ammo > 1)
+            _ammo++;
+            if (_ammo > _maxAmmo)
             {
+                _ammo = _maxAmmo;
                 Debug.Log("Max Ammo");
                 return;
             }
         }
     }
 
+    private void StartDragging(GameObject target, Vector3 hitPoint)
+    {
+        _draggedObject = target;
+        _dragOffset = target.transform.position - hitPoint;
+    }
+    
+
+    private void SelectStraw(GameObject straw)
+    {
+        Debug.Log($"Straw selected: {straw.name}");
+    }
+
+    
     private void ShootBoba()
     {
-        //instantiates a bubble and shoots it in front of player
-        //if hits a gameObject CompareTag("seagull") invoke?. action
-        ammo--;
+        if (_ammo <= 0) { return; } 
+        GameObject boba = Instantiate(_bobaPrefab, _bobaSpawnPoint.position, _bobaSpawnPoint.rotation);
+        Rigidbody bobaRigidbody = boba.GetComponent<Rigidbody>();
+        bobaRigidbody.AddForce(_camera.transform.forward * bobaShootForce, ForceMode.Impulse);
+        _ammo--;
+
+        //if (hits object && objectHit.CompareTag("Seagull"))
+        //if hits a gameObject CompareTag("seagull")
+        //invoke?. seagull action takeDamage
+        //Destroy(boba)
         
+        Destroy(boba, bobaSpawnTime);
     }
 }
